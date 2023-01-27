da3c04f8634ddfc9171d4e7f24c6ae1e07ede729
KayKilb <infulbloom@msn.com>
2023-01-27 09:34:52 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-/*
- * main - comment
- * Return: puts command
- */
-
 #include<stdio.h>
 
+/**
+ * main - puts command
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts "Programming is like building a multilingual puzzle/n";
-
-return 0;
+	puts("Programming is like building a multilingual puzzle/n");
+	return (0);
 }
