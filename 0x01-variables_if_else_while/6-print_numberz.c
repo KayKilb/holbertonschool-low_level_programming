76f348b92e2c13f46a796625508c147a2c0f6695
KayKilb <infulbloom@msn.com>
2023-01-30 15:20:47 -0800

    prints 10 numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - base numbers up to 10
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+putchar(n);
+putchar('\n');
+
+return (0);
+}
