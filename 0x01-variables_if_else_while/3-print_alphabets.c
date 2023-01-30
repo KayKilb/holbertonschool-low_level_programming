c08bebebff5a09b6dcd9a963336772f36337f047
KayKilb <infulbloom@msn.com>
2023-01-30 14:01:13 -0800

    Aa-Za

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <time.h>
 
-/*
- * main - Entry point for capital and lowercase letters
+/**
+ * main - Will print lower then upper case alphabet
  *
- * Return: Always 0
+ * Return: always 0
  */
 
 int main(void)
