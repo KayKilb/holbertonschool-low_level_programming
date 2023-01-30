1e484c6767ea7d4c0b153d28aa5cb4284f6db059
KayKilb <infulbloom@msn.com>
2023-01-30 09:59:41 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*We will write the alphabet out followed by a new line*/
+
+int main(void)
+{
+char ch;
+
+for
+("ch='a' ; ch<='z'");
+
+{
+putchar(ch);
+ch++;
+}
+putchar('\n')
+return (0);
+}
