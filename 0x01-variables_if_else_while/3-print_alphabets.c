628817891ab96e8b2ad2de42f57cbff86ef8b686
KayKilb <infulbloom@msn.com>
2023-01-30 13:54:23 -0800

    Aa-Za

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point for capital and lowercase letters
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char c;
+
+char c = 'a' || c = 'A';
+while (char c <= 'z' || 'Z')
+
+putchar(c);
+c++;
+
+putchar('\n');
+return (0);
+}
