e6e4884bff3886af2ea4c5d0d3dce6c0ece54598
KayKilb <infulbloom@msn.com>
2023-01-30 20:26:33 -0800

    1 or 0

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,18 @@
 /**
 * _isalpha - a function that checks for alphabetic character
 * @c: single letter
+*
 * Return: 1 if c is a letter (lower or uppercase), else 0
 */
-int _isalpha(char c)
+
+int _isalpha(int c)
 {
 if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+{
 return (1);
+}
 else
+{
 return (0);
+}
 }
