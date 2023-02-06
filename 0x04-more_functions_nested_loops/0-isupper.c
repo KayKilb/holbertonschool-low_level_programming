22fb26b4f16c18dd3132f043060480938225733b
KayKilb <infulbloom@msn.com>
2023-02-05 21:13:16 -0800

    Upper C

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _isupper - is a function that checks for upper case letters
+ * _isupper - is a function that checks for uppercase letters
  * @c: input
  * Return: 1 if C is found, else return 0
  */
