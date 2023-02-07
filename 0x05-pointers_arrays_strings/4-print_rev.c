a0bad49543762dcb49d617067113296547e1d20d
KayKilb <infulbloom@msn.com>
2023-02-06 20:00:31 -0800

    reverse string function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,17 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
-	int length = 0;
+	int fcounter = 0;
+	int i, n;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		i = length;
-		for (length -= 1; length >= 0; length--)
-		{
-			_putchar(s[length]);
-		}
+		fcounter++;
 	}
-		_putchar('\n');
+
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
 }
