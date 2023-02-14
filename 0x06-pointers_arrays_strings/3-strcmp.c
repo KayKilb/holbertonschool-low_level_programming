04f4d182c5cf5d58528cf54de1cedfaaa89f2eeb
KayKilb <infulbloom@msn.com>
2023-02-13 20:08:54 -0800

    15, -15 or 0

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _strcmp - Function comparing two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: 0 for Success
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
