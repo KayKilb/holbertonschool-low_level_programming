eab3c26a16fe64d8eca07412d1b6b924ebaef743
KayKilb <infulbloom@msn.com>
2023-02-15 12:52:57 -0800

    0x00 & 0x01's

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - Is the Entry Point
+ * @s: the pointer destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (; i < n; i++)
+		s[i] = b;
+	return (s);
+}
