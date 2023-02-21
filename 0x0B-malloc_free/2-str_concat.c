58e5be57deca145330af6eb42ddae80d2600a13c
KayKilb <infulbloom@msn.com>
2023-02-20 18:45:35 -0800

    concat 2 strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,32 +10,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i, j;
-	char *array;
+	char *s;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
-
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 	}
 
-	array = (char *)malloc(((i + j) + 1) * sizeof(char));
+	s = (char *)malloc(((i + j) + 1) * sizeof(char));
 
-	if (array == NULL)
+	if (s == NULL)
 		return (NULL);
-
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		array[i] = s1[i];
+		s[i] = s1[i];
 	}
 	for (j = 0; s2[j] != '\0'; j++)
 	{
-		array[i] = s2[j];
+		s[i] = s2[j];
 		i++;
 	}
-	return (array);
+	return (s);
 }
