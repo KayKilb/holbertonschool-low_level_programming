56b29b063402f3dfd97a86820e24ab5b5a03a52f
KayKilb <infulbloom@msn.com>
2023-03-04 11:20:48 -0800

    creating new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int get_len(int i);
+char *str_cpy(char *dest, char *src);
 
 #endif /*DOG_H*/
