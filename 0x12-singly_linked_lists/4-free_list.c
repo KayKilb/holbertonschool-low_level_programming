f7567b08c442bb07e9a36ea05ce57404f266fbad
KayKilb <infulbloom@msn.com>
2023-03-19 22:42:20 -0700

    updated code to omit excess includes

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * free_list - func that frees list_t
- * @head: struct
- * Return - free
+ * free_list - frees list_t
+ *@head: points to head
  */
-void free_list (list_t *head)
+void free_list(list_t *head)
 {
 	if (head == NULL)
 		return;
-	if (head ->next != NULL)
+
+	if (head->next != NULL)
 		free_list(head->next);
 	free(head->str);
 	free(head);
