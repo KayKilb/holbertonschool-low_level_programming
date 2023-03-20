6658a8ae10e8df52c159c82a047657bea62fc190
KayKilb <infulbloom@msn.com>
2023-03-20 12:47:00 -0700

    updated code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,40 +5,36 @@
  * @head: head
  * @idx: index
  * @n: data
- * Return: 0
+ * Return: address of new node, or NULL upon failure
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *temp, *new;
 	unsigned int count = 0;
 
-	listint_t *new;
-	listint_t *temp;
-
-	if (*head == NULL)
-		return (NULL);
-
-	temp = *head;
-
-	while (temp != NULL)
+	if (idx == 0)
 	{
-		if (count == idx - 1)
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			else
-			{
-				new->n = n;
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-		}
-		if (temp->next != NULL)
-			temp = temp->next;
+		add_nodeint(head, n);
+		return (NULL);
+	}
+	temp = *head;
+	while (count < idx - 1)
+	{
+		if (!(temp->next))
+			return (NULL);
+
+		temp = temp->next;
 		count++;
 	}
-	return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		free(temp);
+		return (NULL);
+	}
+	new->next = temp->next;
+	temp->next = new;
+	new->n = n;
+
+	return (new);
 }
