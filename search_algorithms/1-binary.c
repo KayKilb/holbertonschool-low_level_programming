95907147fc4a6700d5d6e009577f57845bd779ee
KayKilb <115676011+KayKilb@users.noreply.github.com>
2023-12-21 19:45:51 -0600

    fixed trailing whitespace

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,12 +2,12 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a sorted array of 
+ * binary_search - searches for a value in a sorted array of
  * integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- * Return: first index where value is located, or -1 if value 
+ * Return: first index where value is located, or -1 if value
  * is not present in array or if array is NULL
  */
 
